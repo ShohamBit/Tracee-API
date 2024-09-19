ccad119f1a64a72b6828c344c779061618e8e342
ShohamBit <shohambit@gmail.com>
2024-09-19 08:49:28 +0000

    added cobra

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # Tracee-API
+
+you can run tracee with an api written in `grpc-go`.
+
+you can run tracee with a grpc option and then you can run tracee as an api.
+
+## About Tracee API
+
+you can config tracee api as you need it, for quick start view the rest of the docs
+
+
+## Quick Start
+
+you can run Tracee  api in 3 different ways but all of them are the same structure
+
+first write a config file with the grpc section
+
+```yaml
+grpc:
+    - tcp:50051
+```
+run this config file in your prefer way
+
+### Commend Line
+
+```bash
+tracee --config /path/to/config
+```
+
+### Docker
+
+```bash
+docker run tracee --config /path/to/config
+```
+
+### Kubernetes
+
+## Tracee API Client
+
+After you run tracee with grpc option on, you need to tracee client.
+
+you can tracee client like
+
+```bash
+how to run tracee client 
+```
+
+
+
+
+
---
+++ init_grpc.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Check if protoc-gen-go is installed, if not install it
+if ! command -v protoc-gen-go &> /dev/null
+then
+    echo "protoc-gen-go not found, installing..."
+    go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+else
+    echo "protoc-gen-go is already installed."
+fi
+
+# Check if protoc-gen-go-grpc is installed, if not install it
+if ! command -v protoc-gen-go-grpc &> /dev/null
+then
+    echo "protoc-gen-go-grpc not found, installing..."
+    go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+else
+    echo "protoc-gen-go-grpc is already installed."
+fi
+
+# Add Go binaries to PATH
+export PATH="$PATH:$(go env GOPATH)/bin"
+
+# Directory containing .proto files
+TRACEE_PROTOS="../../api/v1beta1/*.proto"
+# Run protoc for all .proto files
+    protoc \
+        --proto_path=.
+		--go_out=. \
+		--go_opt=paths=source_relative \
+		--go-json_out=orig_name=true,paths=source_relative:. \
+		--go-grpc_out=. \
+		--go-grpc_opt=paths=source_relative $TRACEE_PROTOS
+
+# Check if the protoc command succeeded
+if [ $? -eq 0 ]; then
+    echo "Protoc command completed successfully."
+    notify-send "Protoc Command" "Protoc command completed successfully."
+else
+    echo "Protoc command failed."
+    notify-send "Protoc Command" "Protoc command failed."
+fi
