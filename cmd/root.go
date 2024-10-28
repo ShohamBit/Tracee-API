ecaebcc811a4fd9b09e1b4385e7ba1336e72dd27
ShohamBit <shohambit@gmail.com>
2024-10-28 20:08:33 +0000

    added support for unix socket over tcp and make it default

--- cmd/root.go
+++ cmd/root.go
@@ -9,13 +9,13 @@ import (
 )
 
 var (
-	DefaultIP   = "localhost"
-	DefaultPort = "4466"
-	TCS         client.ServiceClient    // tracee service client
-	TCD         client.DiagnosticClient // tracee diagnostic  client
-	serverInfo  client.ServerInfo       = client.ServerInfo{
-		IP:   DefaultIP,
-		Port: DefaultPort,
+	TCS        client.ServiceClient    // tracee service client
+	TCD        client.DiagnosticClient // tracee diagnostic  client
+	serverInfo client.ServerInfo       = client.ServerInfo{
+		ConnectionType: client.PROTOCOL_UNIX,
+		UnixSocketPath: client.SOCKET,
+		IP:             client.DefaultIP,
+		Port:           client.DefaultPort,
 	}
 
 	rootCmd = &cobra.Command{
@@ -23,6 +23,15 @@ var (
 		Short: "TraceeClient is a CLI tool for tracee",
 		Long:  "Tracee client is the client for tracee api server.",
 		Run: func(cmd *cobra.Command, args []string) {
+			// Check if connection type is TCP; if not, disable IP and Port flags
+			if serverInfo.ConnectionType == client.PROTOCOL_UNIX {
+				// Clear IP and Port as they're not used with Unix socket
+				serverInfo.IP = client.DefaultIP
+				serverInfo.Port = client.DefaultPort
+			} else if serverInfo.ConnectionType != client.PROTOCOL_TCP {
+				cmd.Println("Invalid connection type. Use 'tcp' or 'unix'.")
+				os.Exit(1)
+			}
 			cmd.Help()
 		},
 	}
@@ -38,8 +47,23 @@ func init() {
 	rootCmd.AddCommand(streamEventsCmd)
 
 	//flags
-	rootCmd.PersistentFlags().StringVarP(&serverInfo.IP, "ip", "i", DefaultIP, "IP to connect to the remote server")
-	rootCmd.PersistentFlags().StringVarP(&serverInfo.Port, "port", "p", DefaultPort, "Port to connect to the remote server")
+	rootCmd.PersistentFlags().StringVarP(&serverInfo.ConnectionType, "connectionType", "c", client.PROTOCOL_UNIX, "Connection type (unix|tcp)")
+	rootCmd.RegisterFlagCompletionFunc("connectionType", func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+		return []string{client.PROTOCOL_TCP, client.PROTOCOL_UNIX}, cobra.ShellCompDirectiveNoFileComp
+	})
+	rootCmd.PersistentFlags().StringVar(&serverInfo.UnixSocketPath, "socketPath", client.SOCKET, "Path of the unix socket")
+	// TODO: make this flag available only when the connection type is tcp
+	rootCmd.PersistentFlags().StringVarP(&serverInfo.IP, "ip", "i", client.DefaultIP, "IP to connect to the remote server")
+	rootCmd.PersistentFlags().StringVarP(&serverInfo.Port, "port", "p", client.DefaultPort, "Port to connect to the remote server")
+
+	// Adjust flags based on connection type
+	rootCmd.PersistentPreRun = func(cmd *cobra.Command, args []string) {
+		// Disable IP and Port flags for unix connections
+		if serverInfo.ConnectionType != client.PROTOCOL_TCP {
+			cmd.PersistentFlags().Lookup("ip").Hidden = true
+			cmd.PersistentFlags().Lookup("port").Hidden = true
+		}
+	}
 
 }
 
--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,28 +1,63 @@
 package client
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"net"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
 // github.com/ShohamBit/TraceeClient holds the gRPC connection and service client.
+const (
+	// unix socket
+	PROTOCOL_UNIX = "unix"
+	PROTOCOL_TCP  = "tcp"
+	SOCKET        = "/tmp/grpc.sock"
+	DefaultIP     = "localhost"
+	DefaultPort   = "4466"
+)
 
 type ServerInfo struct {
-	IP   string
-	Port string
+	ConnectionType string // Field to specify connection type (e.g., "unix" or "tcp")
+	UnixSocketPath string // Path for the Unix socket, if using Unix connection
+	IP             string
+	Port           string
 }
 
+// this function use grpc to connect the server
+// it can connect to the server with tcp stream or unix socket
 func connectToServer(serverInfo ServerInfo) (*grpc.ClientConn, error) {
-	//fmt.Printf("Connecting to server on %s...\n", serverInfo.Address())
-
-	//connect to server
 	var opts []grpc.DialOption
 	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	conn, err := grpc.NewClient(serverInfo.IP+":"+serverInfo.Port, opts...)
+
+	// Use switch case to determine connection type
+	var conn *grpc.ClientConn
+	var err error
+
+	switch serverInfo.ConnectionType {
+	case PROTOCOL_UNIX:
+		// Dial a Unix socket
+		conn, err = grpc.NewClient(
+			serverInfo.UnixSocketPath,
+			append(opts, grpc.WithContextDialer(func(ctx context.Context, addr string) (net.Conn, error) {
+				return net.Dial("unix", addr)
+			}))...,
+		)
+
+	case PROTOCOL_TCP:
+		// Dial a TCP address
+		address := fmt.Sprintf("%s:%s", serverInfo.IP, serverInfo.Port)
+		conn, err = grpc.NewClient(address, opts...)
+
+	default:
+		return nil, fmt.Errorf("unsupported connection type: %s", serverInfo.ConnectionType)
+	}
+
 	if err != nil {
-		log.Fatalf("server is down %v", err)
+		log.Fatalf("failed to connect to server: %v", err)
 		return nil, err
 	}
 	return conn, nil
