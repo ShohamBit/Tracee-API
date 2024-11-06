c1b317f5a4e902c63771741c9aae56d4d0016af7
ShohamBit <shohambit@gmail.com>
2024-11-06 11:46:46 +0000

    make test files unavillable until design and commands changes

--- cmd/plugin_test.go
+++ cmd/plugin_test.go
@@ -1,55 +1,55 @@
 package cmd
 
-import (
-	"bytes"
-	"testing"
-	"time"
+// import (
+// 	"bytes"
+// 	"testing"
+// 	"time"
 
-	"github.com/ShohamBit/traceectl/pkg/mock"
-	"github.com/ShohamBit/traceectl/pkg/models"
-)
+// 	"github.com/ShohamBit/traceectl/pkg/mock"
+// 	"github.com/ShohamBit/traceectl/pkg/models"
+// )
 
-var pluginTests = []models.TestCase{
-	{
-		Name:           "No plugin subcommand",
-		Args:           []string{"plugin"},
-		ExpectedOutput: "Error: requires at least 1 arg(s), only received 0\n", // Update expected output
-	},
-}
+// var pluginTests = []models.TestCase{
+// 	{
+// 		Name:           "No plugin subcommand",
+// 		Args:           []string{"plugin"},
+// 		ExpectedOutput: "Error: requires at least 1 arg(s), only received 0\n", // Update expected output
+// 	},
+// }
 
-func TestPluginCmd(t *testing.T) {
-	// Start the mock server
-	mockServer, err := mock.StartMockServer()
-	if err != nil {
-		t.Fatalf("Failed to start mock server: %v", err)
-	}
-	defer mockServer.Stop() // Ensure the server is stopped after the test
+// func TestPluginCmd(t *testing.T) {
+// 	// Start the mock server
+// 	mockServer, err := mock.StartMockServer()
+// 	if err != nil {
+// 		t.Fatalf("Failed to start mock server: %v", err)
+// 	}
+// 	defer mockServer.Stop() // Ensure the server is stopped after the test
 
-	// Wait for the server to start
-	time.Sleep(100 * time.Millisecond)
+// 	// Wait for the server to start
+// 	time.Sleep(100 * time.Millisecond)
 
-	for _, test := range pluginTests {
-		t.Run(test.Name, func(t *testing.T) {
-			// Capture output
-			var buf bytes.Buffer
-			rootCmd := GetRootCmd()
-			rootCmd.SetOut(&buf)
-			rootCmd.SetErr(&buf)
+// 	for _, test := range pluginTests {
+// 		t.Run(test.Name, func(t *testing.T) {
+// 			// Capture output
+// 			var buf bytes.Buffer
+// 			rootCmd := GetRootCmd()
+// 			rootCmd.SetOut(&buf)
+// 			rootCmd.SetErr(&buf)
 
-			// Set arguments for the test
-			rootCmd.SetArgs(test.Args)
+// 			// Set arguments for the test
+// 			rootCmd.SetArgs(test.Args)
 
-			// Execute the command
-			if err := rootCmd.Execute(); err != nil {
-				t.Error(t, err)
-			}
+// 			// Execute the command
+// 			if err := rootCmd.Execute(); err != nil {
+// 				t.Error(t, err)
+// 			}
 
-			// Validate output and error (if any)
-			output := buf.String()
+// 			// Validate output and error (if any)
+// 			output := buf.String()
 
-			if output != test.ExpectedOutput {
-				t.Errorf("Expected output: %s, got: %s", test.ExpectedOutput, output)
-			}
-		})
-	}
-}
+// 			if output != test.ExpectedOutput {
+// 				t.Errorf("Expected output: %s, got: %s", test.ExpectedOutput, output)
+// 			}
+// 		})
+// 	}
+// }
